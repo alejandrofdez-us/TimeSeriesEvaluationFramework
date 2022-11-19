4db2c62e67d070d41c97afd030bbebfa6cc4bf30
alejandrofdez-us <afdez@us.es>
2022-11-19 10:51:12 +0100

    Fixing bug when ori_data_lenght is the same as sequence length.

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -515,7 +515,11 @@ def normalize_start_time_to_zero(sample):
 
 
 def get_ori_data_sample(seq_len, ori_data):
-    ori_data_sample_start = random.randrange(0, len(ori_data) - seq_len)
+    if len(ori_data) > seq_len:
+        ori_data_sample_start = random.randrange(0, len(ori_data) - seq_len)
+    else: # seq_len is the full ori_data_length
+        ori_data_sample_start = 0
+
     ori_data_sample_end = ori_data_sample_start + seq_len
     ori_data_sample = ori_data[ori_data_sample_start:ori_data_sample_end]
     return ori_data_sample

790a7cc98beb8df74847d1cc50c6885c627d0e90
Felipe Escalera González <felescgon@alum.us.es>
2023-07-07 12:45:42 +0200

    Removing unused variables in PlotFactory

--- plots/plot_factory.py
+++ plots/plot_factory.py
@@ -14,24 +14,16 @@ class Singleton(type):
 
 class PlotFactory(metaclass=Singleton):
     def __init__(self, figure_names_to_be_generated):
-        self.figures_names_to_be_generated = figure_names_to_be_generated
         self.plots_to_be_generated = self.__get_plots_to_be_generated(figure_names_to_be_generated)
         self.figures_requires_all_samples = self.__get_figures_that_requires_all_samples(figure_names_to_be_generated)
-        self.ts1_windows = None
 
-    def __get_plots_to_be_generated(self, figure_names_to_be_generated):
+    @staticmethod
+    def __get_plots_to_be_generated(figure_names_to_be_generated):
         available_plots = PlotFactory.find_available_figures()
         plots_to_be_generated = {plot_name: plot for plot_name, plot in available_plots.items() if
                                  plot_name in figure_names_to_be_generated}
         return plots_to_be_generated
-
-    def __get_figures_that_requires_all_samples(self, figures_to_be_generated):
-        return [figure_name for figure_name in figures_to_be_generated if
-                self.__figure_requires_all_samples(figure_name)]
-
-    def __figure_requires_all_samples(self, figure_name):
-        return self.plots_to_be_generated[figure_name].requires_all_samples()
-
+    
     @staticmethod
     def find_available_figures():
         return find_available_classes(os.path.dirname(os.path.abspath(__file__)), Plot, 'plots')
@@ -43,3 +35,10 @@ class PlotFactory(metaclass=Singleton):
                 figure_names_to_be_generated = ['2d', 'deltas', 'dtw', 'pca', 'tsne']
             PlotFactory._instance = PlotFactory(figure_names_to_be_generated)
         return PlotFactory._instance
+
+    def __get_figures_that_requires_all_samples(self, figures_to_be_generated):
+        return [figure_name for figure_name in figures_to_be_generated if
+                self.__figure_requires_all_samples(figure_name)]
+
+    def __figure_requires_all_samples(self, figure_name):
+        return self.plots_to_be_generated[figure_name].requires_all_samples()

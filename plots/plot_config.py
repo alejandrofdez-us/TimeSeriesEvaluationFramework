8bfb09887e097af180e419e3ec073727f101f9cb
alejandrofdez-us <afdez@us.es>
2023-07-07 11:24:47 +0200

    Removed unused properties

--- plots/plot_config.py
+++ plots/plot_config.py
@@ -1,10 +1,6 @@
 class PlotConfig:
-    # FIXME: eliminar window_selection_metric, stride
-    def __init__(self, figures=None, timestamp_frequency_seconds=1, stride=1,
-                 window_selection_metric='dtw'):
+    def __init__(self, figures=None, timestamp_frequency_seconds=1):
         if figures is None:
             figures = ['2d', 'deltas', 'dtw', 'pca', 'tsne']
-        self.stride = stride
-        self.window_selection_metric = window_selection_metric
         self.figures = figures
         self.timestamp_frequency_seconds = timestamp_frequency_seconds

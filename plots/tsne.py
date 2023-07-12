5accc3ce7050a1e52caa9b9160efe056861e1529
alejandrofdez-us <afdez@us.es>
2023-07-12 17:54:04 +0200

    refactored

--- plots/tsne.py
+++ plots/tsne.py
@@ -1,3 +1,5 @@
+import itertools
+
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,36 +11,40 @@ class Tsne(DimensionalityReduction):
     def requires_all_samples():
         return True
 
-    def __init__(self):
+    def __init__(self):  # FIXME: eliminar ya que no hace nada?
         super().__init__()
 
     def compute(self, core, filename):
         super().compute(core, filename)
-        all_ts_prepared = np.concatenate((self.ts1_prepared, self.ts2_prepared), axis=0)
-        colors = ["red" for _ in range(len(self.ts1_prepared))] + ["blue" for _ in range(len(self.ts2_prepared))]
+        ts1_ts2_reduced_dimensions_concatenated = np.concatenate(
+            (self.ts1_reduced_dimensions, self.ts2_reduced_dimensions), axis=0)
+        colors = ["red" for _ in range(len(self.ts1_reduced_dimensions))] + ["blue" for _ in
+                                                                             range(len(self.ts2_reduced_dimensions))]
         plot_array = []
-        perplexities = {5, 10, min(40,
-                                   len(self.ts2_prepared))}  # FIXME: Check what would be a valid perplexity when reduced number of samples. According to documentation "The perplexity must be less than the number of samples."
-        for perplexity in perplexities:
-            plot_array.append(self.__compute_tsne(len(self.ts1_prepared), colors, perplexity, all_ts_prepared, 300,
-                                                  f'iter_300-perplexity_{perplexity}'))
-            plot_array.append(self.__compute_tsne(len(self.ts1_prepared), colors, perplexity, all_ts_prepared, 1000,
-                                                  f'iter_1000-perplexity_{perplexity}'))
+        perplexities = [5, 10, min(40, len(self.ts1_reduced_dimensions))]
+        perplexities = list(filter(lambda p: p <= len(self.ts1_reduced_dimensions), perplexities))
+        iterations = [300, 1000]
+
+        for perplexity, n_iterations in itertools.product(perplexities, iterations):
+            tsne_embedding = self.__compute_tsne(perplexity,
+                                                 ts1_ts2_reduced_dimensions_concatenated, n_iterations)
+            tsne_plot = self.__tsne_ploting(len(self.ts1_reduced_dimensions), colors, tsne_embedding,
+                                            f'iter_{n_iterations}-perplexity_{perplexity}')
+            plot_array.append(tsne_plot)
         return plot_array
 
-    def __compute_tsne(self, anal_sample_no, colors, perplexity, ts1_prepared_final, iterations, filename):
+    def __compute_tsne(self, perplexity, ts1_ts2_reduced_dimensions_concatenated, iterations):
         tsne = TSNE(n_components=2, verbose=0, perplexity=perplexity, n_iter=iterations)
-        tsne_results = tsne.fit_transform(ts1_prepared_final)
-        return self.__tsne_ploting(anal_sample_no, colors, tsne_results, filename)
+        return tsne.fit_transform(ts1_ts2_reduced_dimensions_concatenated)
 
-    def __tsne_ploting(self, anal_sample_no, colors, tsne_results, filename):
+    def __tsne_ploting(self, anal_sample_no, colors, tsne_embedding, title):
         fig, ax = plt.subplots(1)
-        plt.scatter(tsne_results[:anal_sample_no, 0], tsne_results[:anal_sample_no, 1],
+        plt.scatter(tsne_embedding[:anal_sample_no, 0], tsne_embedding[:anal_sample_no, 1],
                     c=colors[:anal_sample_no], alpha=0.2, label="TS_1")
-        plt.scatter(tsne_results[anal_sample_no:, 0], tsne_results[anal_sample_no:, 1],
+        plt.scatter(tsne_embedding[anal_sample_no:, 0], tsne_embedding[anal_sample_no:, 1],
                     c=colors[anal_sample_no:], alpha=0.2, label="TS_2")
         ax.legend()
-        plt.title(f't_SNE_{filename}')
+        plt.title(f't_SNE_{title}')
         plt.xlabel('x_tsne')
         plt.ylabel('y_tsne')
         plt.close("all")

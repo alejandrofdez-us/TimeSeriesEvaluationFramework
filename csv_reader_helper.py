f1d066775210fcf7a7d7314bc3a781fe0b8b62c8
Felipe Escalera González <felescgon@alum.us.es>
2023-07-13 12:43:09 +0200

    Detection of csv encoding and saving .json in UTF-8

--- csv_reader_helper.py
+++ csv_reader_helper.py
@@ -1,7 +1,14 @@
 import csv
 import os
+import chardet
 import numpy as np
 from natsort import natsorted
+
+def detect_encoding(file_path):
+    with open(file_path, 'rb') as file:
+        raw_data = file.read()
+        result = chardet.detect(raw_data)
+        return result['encoding'], result['confidence']
 
 
 def read_header_from_csv(filename, ts_delimiter, has_header):
@@ -9,12 +16,13 @@ def read_header_from_csv(filename, ts_delimiter, has_header):
         header = np.genfromtxt(filename, delimiter=ts_delimiter, names=has_header, max_rows=1, dtype=str).dtype.names
     else:
         first_row = np.loadtxt(filename, delimiter=ts_delimiter, max_rows=1)
-        header = ["column-" + str(i) for i in range(len(first_row))]
+        header = ['column-' + str(i) for i in range(len(first_row))]
     return header
 
 
 def detect_line_delimiter(filename):
-    with open(filename, "r", newline="") as file:
+    encoding, _ = detect_encoding(filename)
+    with open(filename, 'r', newline='', encoding=encoding) as file:
         ts_delimiter = csv.Sniffer().sniff(file.readline()).delimiter
 
     return ts_delimiter
@@ -31,7 +39,7 @@ def load_ts_from_csv(filename, has_header=None):
 
 def load_ts_from_path(path, header_ts1, has_header=None):
     if not os.path.exists(path):
-        raise FileNotFoundError(f" Path {path} does not exist")
+        raise FileNotFoundError(f' Path {path} does not exist')
     time_series = {}
     if os.path.isfile(path):
         ts2, header_ts2 = load_ts_from_csv(path, has_header)
@@ -42,7 +50,7 @@ def load_ts_from_path(path, header_ts1, has_header=None):
             files = [file for file in files if not file.startswith('.')]
             files = natsorted(files)
             for file in files:
-                ts2, header_ts2 = load_ts_from_csv(f"{path}/{file}", has_header)
+                ts2, header_ts2 = load_ts_from_csv(f'{path}/{file}', has_header)
                 check_headers(header_ts1, header_ts2)
                 time_series[file] = ts2
 
@@ -51,4 +59,4 @@ def load_ts_from_path(path, header_ts1, has_header=None):
 
 def check_headers(header_ts1, header_ts2):
     if header_ts1 != header_ts2:
-        raise ValueError("All time series must have the same header column names.")
+        raise ValueError('All time series must have the same header column names.')
--- main.py
+++ main.py
@@ -16,22 +16,18 @@ from csv_reader_helper import load_ts_from_csv, load_ts_from_path
 def warning_handler(message, category, filename, lineno, file=None, line=None):
     print(message, file=sys.stderr)
 
-
 warnings.showwarning = warning_handler
 
 
 def main(arguments):
-    try:
-        ts1, header_ts1 = load_ts_from_csv(arguments.time_series_1_filename, arguments.header)
-        ts2_dict = load_ts_from_path(arguments.time_series_2_path, header_ts1, arguments.header)
-        core_config = __create_core_config(arguments, list(ts2_dict.keys()), header_ts1)
-        core = Core(ts1, list(ts2_dict.values()), core_config)
-        if core_config.metric_config.metrics:
-            __compute_metrics(core)
-        if core_config.plot_config.figures:
-            __compute_figures(core)
-    except ValueError as error:  # FIXME: Value error?
-        print("Error: ", error)
+    ts1, header_ts1 = load_ts_from_csv(arguments.time_series_1_filename, arguments.header)
+    ts2_dict = load_ts_from_path(arguments.time_series_2_path, header_ts1, arguments.header)
+    core_config = __create_core_config(arguments, list(ts2_dict.keys()), header_ts1)
+    core = Core(ts1, list(ts2_dict.values()), core_config)
+    if core_config.metric_config.metrics:
+        __compute_metrics(core)
+    if core_config.plot_config.figures:
+        __compute_figures(core)
 
 
 def __compute_figures(core):
@@ -53,7 +49,7 @@ def __compute_metrics(core):
             tqdm_metric_computer_iterator.set_postfix(file=filename)
             metrics_results[filename] = {}
         metrics_results[filename][metric_name] = computed_metric
-    __save_metrics(json.dumps(metrics_results, indent=4))
+    __save_metrics(json.dumps(metrics_results, indent=4, ensure_ascii=False).encode('utf-8'))
 
 
 def __create_core_config(arguments, ts2_names, header_names):
@@ -70,94 +66,94 @@ def __create_core_config(arguments, ts2_names, header_names):
     return core_config
 
 
-def __save_figures(filename, plot_name, generated_plots, path="results/figures"):
+def __save_figures(filename, plot_name, generated_plots, path='results/figures'):
     for plot in generated_plots:
         dir_path = __create_directory(filename, path, plot_name)
-        plot[0].savefig(f"{dir_path}{plot[0].axes[0].get_title()}.pdf", format="pdf", bbox_inches="tight")
+        plot[0].savefig(f'{dir_path}{plot[0].axes[0].get_title()}.pdf', format='pdf', bbox_inches='tight')
 
 
 def __create_directory(filename, path, plot_name):
     if plot_name in PlotFactory.get_instance().figures_requires_all_samples:
-        dir_path = f"{path}/{plot_name}/"
+        dir_path = f'{path}/{plot_name}/'
     else:
         original_filename = os.path.splitext(filename)[0]
-        dir_path = f"{path}/{original_filename}/{plot_name}/"
+        dir_path = f'{path}/{original_filename}/{plot_name}/'
     os.makedirs(dir_path, exist_ok=True)
     return dir_path
 
 
-def __save_metrics(computed_metrics, path="results/metrics"):
-    os.makedirs(f"{path}", exist_ok=True)
-    with open(f"{path}/results.json", "w") as file:
-        file.write(computed_metrics)
+def __save_metrics(computed_metrics, path='results/metrics'):
+    os.makedirs(f'{path}', exist_ok=True)
+    with open(f'{path}/results.json', 'w', encoding='utf-8') as file:
+        file.write(computed_metrics.decode('utf-8'))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     available_metrics = MetricFactory.find_available_metrics().keys()
     available_figures = PlotFactory.find_available_figures().keys()
     parser = argparse.ArgumentParser(
-        usage="python main.py -ts1 path_to_file_1 -ts2_path path_to_files_2 [--metrics] [js ...] [--figures] [tsne ...] \
-            [--header] [--timestamp_frequency_seconds] 300 [--stride] 2 [--window_selection_metric] metric_name"
+        usage='python main.py -ts1 path_to_file_1 -ts2_path path_to_files_2 [--metrics] [js ...] [--figures] [tsne ...] \
+            [--header] [--timestamp_frequency_seconds] 300 [--stride] 2 [--window_selection_metric] metric_name'
     )
     parser.add_argument(
-        "-ts1",
-        "--time_series_1_filename",
-        help="<Required> Include a csv filename that represents a time series. If ts1 is bigger than time series in ts2_path, it will be split in windows.",
+        '-ts1',
+        '--time_series_1_filename',
+        help='<Required> Include a csv filename that represents a time series. If ts1 is bigger than time series in ts2_path, it will be split in windows.',
         type=str,
         required=True,
     )
     parser.add_argument(
-        "-ts2",
-        "--time_series_2_path",
-        help="<Required> Include the path to a csv file or a directory with csv files, each one representing time series.",
+        '-ts2',
+        '--time_series_2_path',
+        help='<Required> Include the path to a csv file or a directory with csv files, each one representing time series.',
         type=str,
         required=True,
     )
     parser.add_argument(
-        "-m",
-        "--metrics",
-        nargs="+",
-        help="<Optional> Include metrics to be computed as a list separated by spaces.",
+        '-m',
+        '--metrics',
+        nargs='+',
+        help='<Optional> Include metrics to be computed as a list separated by spaces.',
         choices=available_metrics,
         required=False,
     )
     parser.add_argument(
-        "-f",
-        "--figures",
-        nargs="+",
-        help="<Optional> Include figure names to be generated as a list separated by spaces.",
+        '-f',
+        '--figures',
+        nargs='+',
+        help='<Optional> Include figure names to be generated as a list separated by spaces.',
         choices=available_figures,
         required=False,
     )
     parser.add_argument(
-        "-head",
-        "--header",
-        help="<Optional> If the time-series includes a header row.",
+        '-head',
+        '--header',
+        help='<Optional> If the time-series includes a header row.',
         required=False,
-        action="store_true",
+        action='store_true',
     )
     parser.add_argument(
-        "-ts_freq_secs",
-        "--timestamp_frequency_seconds",
-        help="<Optional> Include the frequency in seconds in which samples were taken.",
+        '-ts_freq_secs',
+        '--timestamp_frequency_seconds',
+        help='<Optional> Include the frequency in seconds in which samples were taken.',
         required=False,
         default=1,
         type=int,
     )
     parser.add_argument(
-        "-strd",
-        "--stride",
-        help="<Optional> Include the stride to be used in moving windows over samples.",
+        '-strd',
+        '--stride',
+        help='<Optional> Include the stride to be used in moving windows over samples.',
         required=False,
         default=1,
         type=int,
     )
     parser.add_argument(
-        "-w_select_met",
-        "--window_selection_metric",
-        help="<Optional> Include the chosen metric used to pick the best window in the first time series.",
+        '-w_select_met',
+        '--window_selection_metric',
+        help='<Optional> Include the chosen metric used to pick the best window in the first time series.',
         required=False,
-        default="dtw",
+        default='dtw',
         type=str,
     )
     args = parser.parse_args()
